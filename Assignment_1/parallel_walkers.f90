c8b7d06f25bb41e2deafd97a7a3c779746ae02bf
NicolaPerin <98810769+NicolaPerin@users.noreply.github.com>
2023-04-10 10:28:31 +0200

    Added Fortran program for the first exercise

---
+++ Assignment_1/parallel_walkers.f90
@@ -0,0 +1,102 @@
+program parallel_walkers
+    use mpi ! who is this
+    use functions
+    use write
+    use iso_fortran_env, only: int64
+    implicit none
+    integer :: i, j, N, nruns, clock, ierr, rank, size, sizer, rest = 0
+    integer(kind=int64) xN
+    integer, dimension(:), allocatable :: seed
+    integer(kind=int64), dimension(:), allocatable :: all_seeds, P_N, sum_P_N, x_i, x2_i, sum_x_i, sum_x2_i
+    real :: start_time, end_time, comm, comp, writ
+    real(kind=dp), dimension(:), allocatable :: p, r
+
+    character(len=32) :: arg ! We like to use command line arguments
+    call get_command_argument(1, arg); read(arg, *) N
+    call get_command_argument(2, arg); read(arg, *) nruns
+
+    ! Initialize the MPI environment
+    call MPI_Init(ierr)
+    call MPI_Comm_rank(MPI_COMM_WORLD, rank, ierr)
+    call MPI_Comm_size(MPI_COMM_WORLD, size, ierr)
+
+    ! If the number of runs is not a multiple of the number of processes, 
+    ! the remainder is distributed across the first processes
+    if ( rank < mod(nruns, size)) rest = 1
+
+    ! This is a pure MPI implementation -> Distributed memory! (Shared memory is a nightmare)
+    ! Allocate a bunch of stuff (each process does it on its own memory)
+    call random_seed(sizer)
+    allocate(seed(sizer), p(N), r(sizer*nruns), all_seeds(sizer*nruns))
+    allocate(x_i(N), x2_i(N), sum_x_i(N), sum_x2_i(N))
+    allocate(P_N(-N:N), sum_P_N(-N:N))
+
+    x_i = 0; x2_i = 0; sum_x_i = 0; sum_x2_i = 0; P_N = 0; sum_P_N = 0 ! Init everybody to zero
+
+    call system_clock(count=clock) ! Set the seed using the clock (O(1))
+    seed = clock + 37 * (/ (i - 1, i = 1, sizer) /) ! The 37 is still a mistery to this day
+    call random_seed(put=seed)
+
+    call random_number(r) ! Generate a random array whose elements will be used as seed components (O(nruns))
+    ! Apparently Fortran has list comprehensions but you can't do +=
+    all_seeds = [(floor(sizer * nruns * r(i)), i = 1, sizer * nruns)]
+
+    start_time = MPI_Wtime()
+
+    ! The computation is carried out in parallel by many processes
+    ! This is the heaviest part of the program, having to do nruns * N iterations (easily 10^9-10^10)
+    ! Since we are performing O(N) operations on O(nruns) data, if we assume that 
+    ! both N and nruns can go to infinity then this is a cpu-bound program.
+    do i = 1, nruns/size + rest
+        ! Use a different seed for each run
+        seed = [(all_seeds((i-1)*sizer + j), j = 1, sizer)]
+        call random_seed(put=seed)
+        call random_number(p) ! get a sequence of random numbers
+        xN = 0 ! reset the initial position for each walker
+        do j = 1, N
+            if (p(j) < 0.5_dp) then
+                xN = xN - 1 ! left
+            else
+                xN = xN + 1 ! right
+            end if
+            x_i(j) = x_i(j) + xN
+            x2_i(j) = x2_i(j) + xN**2
+        end do
+        P_N(xN) = P_N(xN) + 1 ! accumulate (only for j = N)
+    end do
+
+    end_time = MPI_Wtime()
+    comp = end_time - start_time
+
+    ! Reduce values on all processes to a single value (their sum) (O(size * N))
+    ! I chose to consider it communication time, even though there is also a computation going on
+    ! Usually, computation time refers to the time spent in performing local computations on each process
+    start_time = MPI_Wtime()
+    call MPI_Reduce(x_i, sum_x_i, N, MPI_DOUBLE_PRECISION, MPI_SUM, 0, MPI_COMM_WORLD, ierr)
+    call MPI_Reduce(x2_i, sum_x2_i, N, MPI_DOUBLE_PRECISION, MPI_SUM, 0, MPI_COMM_WORLD, ierr)
+    call MPI_Reduce(P_N, sum_P_N, 2*N+1, MPI_DOUBLE_PRECISION, MPI_SUM, 0, MPI_COMM_WORLD, ierr)
+    end_time = MPI_Wtime()
+    comm = end_time - start_time
+
+    if (rank == 0) then ! Parallel I/O is not yet an option and besides, we only have to write O(N) elements
+
+        ! Write in files, measure the time it takes to do this as well
+        start_time = MPI_Wtime()
+
+        call WriteAverageWalkers(N, nruns, sum_x_i, sum_x2_i)
+        call WriteProbabilities(N, nruns, sum_P_N)
+        call WriteDelta(N, nruns, sum_x_i, sum_x2_i)
+
+        end_time = MPI_Wtime()
+        writ = end_time - start_time
+        call WriteTimings(size, N, nruns, comp, comm, writ)
+
+    end if
+
+    deallocate(seed, x_i, x2_i, p, sum_x_i, sum_x2_i, r, all_seeds, P_N, sum_P_N)
+
+    call MPI_Finalize(ierr)
+
+end program parallel_walkers
+
+! mpif90 -march=native -O2 -fcheck=all utils.f90 parallel_walkers.f90 -o parallel_walkers
---
+++ Assignment_1/utils.f90
@@ -0,0 +1,97 @@
+module functions
+    use iso_fortran_env, only: int64
+    implicit none
+    integer, parameter :: dp = selected_real_kind(13)
+    real(kind=dp), parameter :: pi = 3.14159265358979323846_dp
+    contains
+    function stirl(z) result(res)
+        integer, intent(in) :: z
+        real(kind=dp) :: res
+        res = sqrt(2.0_dp * pi * real(z,kind=dp)) * (real(z,kind=dp)/exp(1.0_dp))**real(z,kind=dp)
+        if (z == 0) res = 1.0_dp
+    end function stirl
+
+    recursive function fact(k) result(res)
+    integer, intent(in) :: k
+    integer(kind=int64) :: k64 = 0
+    integer(kind=int64) :: res
+    k64 = transfer(k, k64) ! cast to 64 bits integer
+    if (k <= 0) then
+        res = 1_int64
+    else
+        res = k * fact(k-1)
+    end if
+    end function fact
+end module functions
+
+module write
+    use functions
+    use iso_fortran_env, only: int64
+    implicit none
+    contains
+        ! Write the empirical probability distribution P_N (point f of exercise) (O(N))
+        subroutine WriteProbabilities(N, nruns, sum_P_N)
+            integer, intent(in) :: N, nruns
+            integer(kind=int64), dimension(:), allocatable, intent(in) :: sum_P_N ! MUST put allocatable
+            integer :: i
+
+            open(unit=13, file='P_N.txt', status='replace', action='write') ! point g of the exercise
+            if (N < 21) then ! 20! is the max that 64 bit integers allow
+                do i = -N, N
+                    write(13,*) i, real(sum_P_N(i)) / nruns, 0.5_dp**N * fact(N) / (fact((N+i)/2) * fact((N-i)/2))
+                end do
+            else if (N < 100) then ! Use the Stirling approximation
+                do i = -N, N
+                    write(13,*) i, real(sum_P_N(i))/nruns, & ! empirical distribution
+                                0.5_dp**N*stirl(N)/(stirl((N+i)/2)*stirl((N-i)/2))! Using Stirling
+                end do
+            else ! Big N
+                do i = -N, N ! When benchmarking we don't care about the theoretical distribution
+                    write(13,*) i, real(sum_P_N(i)) / nruns
+                end do
+            end if
+            close(13)
+        end subroutine WriteProbabilities
+
+        subroutine WriteAverageWalkers(N, nruns, sum_x_i, sum_x2_i)
+            integer, intent(in) :: N, nruns
+            integer(kind=int64), dimension(:), allocatable, intent(in) :: sum_x_i, sum_x2_i
+            integer :: i
+            real(kind=dp) :: avg, delta
+
+            open(unit=10, file='avg_walkers.txt', status='replace', action='write')
+            do i = 1, N ! O(N)
+                avg = real(sum_x_i(i),kind=dp) / nruns
+                delta = real(sum_x2_i(i),kind=dp) / nruns - avg**2
+                write(10,*) i, avg, delta
+            end do
+            close(10)
+        end subroutine WriteAverageWalkers
+
+        subroutine WriteDelta(N, nruns, sum_x_i, sum_x2_i)
+            integer, intent(in) :: N, nruns
+            integer(kind=int64), dimension(:), allocatable, intent(in) :: sum_x_i, sum_x2_i
+            integer :: i
+            real(kind=dp) :: delta
+
+            ! Calculate the accuracy of the mean square displacement (points c and e of the exercise) (O(1))
+            delta = real(sum_x2_i(N),kind=dp) / nruns - (real(sum_x_i(N),kind=dp) / nruns)**2.0_dp
+            print*, "Delta:", abs(delta / N - 1.0_dp)
+            open(unit=12, file='delta.txt', position='append', action='write')
+            write(12,*) nruns, N, abs(delta / real(N,kind=dp) - 1.0_dp)
+            close(12)
+        end subroutine WriteDelta
+
+        subroutine WriteTimings(size, N, nruns, comp, comm, writ)
+            integer, intent(in) :: size, N, nruns
+            real, intent(in) :: comp, comm, writ
+
+            ! Write the timings in a file (O(1))
+            open(unit=11, file='scalability.txt', position='append', action='write')
+            write(11,*) size, nruns, N, comp, comm, writ
+            close(11)
+            print*, "Computation time:", comp, "s"
+            print*, "Communication time:", comm, "s"
+            print*, "Writing time:", writ, "s"
+        end subroutine WriteTimings
+end module write
