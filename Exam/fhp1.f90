efef001dd8fb26cbee9266dc67a5a64dc42ca054
NicolaPerin <98810769+NicolaPerin@users.noreply.github.com>
2023-06-25 11:04:27 +0200

    Update fhp1.f90

--- Exam/fhp1.f90
+++ Exam/fhp1.f90
@@ -9,15 +9,16 @@ module Variables
     integer(1), parameter :: LE     = 8  ! 00 001000
     integer(1), parameter :: LU     = 16 ! 00 010000
     integer(1), parameter :: RU     = 32 ! 00 100000
+    integer(1), parameter :: B      = 64 ! 01 000000
     integer(1), parameter :: FULL   = 63 ! 00 111111
 
     ! Useful quantities
     integer(1), dimension(6), parameter :: dir = (/RI, RD, LD, LE, LU, RU/)
-    integer(1), parameter :: NUM_RULES = 2**6 ! Size of the lookup table (basically 64 bytes)
-    integer(1), dimension(0: NUM_RULES - 1) :: rules1 = 0, rules2 = 0
+    integer(1), parameter :: NUM_RULES = 2**7 -1 ! Size of the lookup table (basically 64 bytes)
+    integer(1), dimension(0: NUM_RULES) :: rules1 = 0, rules2 = 0
 
     double precision :: start_time, end_time ! For profiling
-    real, parameter :: density = 0.5, SQRT3_OVER2 = sqrt(3.0) / 2.0
+    real, parameter :: density = 0.6, SQRT3_OVER2 = sqrt(3.0) / 2.0
     real, dimension(6), parameter :: ux = (/1.0, 0.5, -0.5, -1.0, -0.5, 0.5/)
     real, dimension(6), parameter :: uy = (/0.0, -SQRT3_OVER2, -SQRT3_OVER2, 0.0, SQRT3_OVER2, SQRT3_OVER2/)
     real :: vx = 0. ! averaged x velocity for every site configuration
@@ -30,6 +31,7 @@ module Variables
     subroutine InitRules() ! Two sets of rules
 
         integer(1) :: i
+        integer(1) :: highBits, lowBits
 
         ! Set default rules: the particle keeps moving unperturbed if no collision
         do i = 0, NUM_RULES - 1
@@ -51,6 +53,14 @@ module Variables
 
         rules2(ior(ior(LU, LD), RI)) = ior(ior(RU, LE), RD) ! rule(21) = 42
         rules2(ior(ior(RU, LE), RD)) = ior(ior(LU, LD), RI) ! rule(42) = 21
+
+        ! Add rules to bounce back at barriers
+        rules1(ior(B,RI)) = ior(B,LE); rules2(ior(B,RI)) = ior(B,LE)
+        rules1(ior(B,RD)) = ior(B,RU); rules2(ior(B,RD)) = ior(B,RU)
+        rules1(ior(B,LD)) = ior(B,LU); rules2(ior(B,LD)) = ior(B,LU)
+        rules1(ior(B,LE)) = ior(B,RI); rules2(ior(B,LE)) = ior(B,RI)
+        rules1(ior(B,LU)) = ior(B,LD); rules2(ior(B,LU)) = ior(B,LD)
+        rules1(ior(B,RU)) = ior(B,RD); rules2(ior(B,RU)) = ior(B,RD)
 
     end subroutine InitRules
 
@@ -104,8 +114,8 @@ module Dynamic
         integer, intent(in) :: Lx, Ly
         integer(1), dimension(:,:), allocatable, intent(inout) :: Lattice
 
-        do i = 0, Lx - 1
-            do j = 0, Ly - 1
+        do i = 1, Lx - 2
+            do j = 0, Ly / 2
                 call random_number(r)
                 if (r < density / 8.0) then
                     Lattice(i, j) = ior(ior(RU, LE), RD)
@@ -113,19 +123,42 @@ module Dynamic
                     Lattice(i, j) = ior(ior(LU, LD), RI)
                 else if (r > density) then
                     Lattice(i, j) = 0
-                else 
+                else
                     Lattice(i, j) = RI
                 end if
             end do
         end do
 
+        do j = 0, Ly - 1
+            Lattice(0,j) = B; Lattice(Lx-1, j) = B
+        end do
+
     end subroutine InitLattice
 
-    subroutine Evolve(Lattice, newLattice, Lx, Ly, avg_vel_site, avg_vel_cell, cell_size)
+    subroutine InitLattice2(Lattice, Lx, Ly)
+        integer :: i, j
+        real :: r
+        integer, intent(in) :: Lx, Ly
+        integer(1), dimension(:,:), allocatable, intent(inout) :: Lattice
+
+        do i = Lx / 2, Lx / 2 + 1
+            do j = 0, Ly / 8
+                Lattice(i,j) = RU
+            end do
+        end do
+
+        do j = 0, Ly - 1
+            Lattice(0,j) = B; Lattice(Lx-1, j) = B
+        end do
+
+    end subroutine InitLattice2
+
+    subroutine Evolve(Lattice, newLattice, Lx, Ly, avg_vel_site, avg_vel_cell, cell_size, do_i_write)
 
         integer :: i, j, k, l
         real :: block_sum_x, block_sum_y, norm
         integer, intent(in) :: Lx, Ly, cell_size
+        logical, intent(in) :: do_i_write
         integer(1), dimension(:,:), allocatable, intent(inout) :: Lattice, newLattice
         real, dimension(:,:,:), allocatable, intent(inout) :: avg_vel_site, avg_vel_cell
 
@@ -142,9 +175,8 @@ module Dynamic
         ! Odd row: (i, j) -> (i, j+1), (i+1, j), (i+1, j-1), (i, j-1), (i-1, j-1), (i-1, j)
         ! Even row:(i, j) -> (i, j+1), (i+1, j+1), (i+1, j), (i, j-1), (i-1, j), (i-1, j+1)
 
-        !$OMP PARALLEL DO DEFAULT(shared) COLLAPSE(2)
-        do j = 0, Ly - 1
-            do i = 0, Lx - 1
+        do i = 0, Lx - 1
+            do j = 0, Ly - 1
                 if (mod(i,2) == 0) then ! even row
                     ! RIGHT
                     newLattice(i,mod(j+1,Ly)) = ior(newLattice(i,mod(j+1,Ly)), iand(lattice(i,j), RI))
@@ -170,7 +202,6 @@ module Dynamic
                 end if
             end do
         end do
-        !$OMP END PARALLEL DO
 
         ! Collision step: handle interactions between particles.
         ! In this step, collisions between particles are resolved according to the
@@ -178,7 +209,6 @@ module Dynamic
         ! based on the configurations of the current lattice site.
         ! We switch rule set between even and odd iterations to prevent chirality (particles turning in circles)
         
-        !$OMP PARALLEL DO DEFAULT(shared) PRIVATE(i, j, k, vx, vy)
         do i = 0, Lx - 1
             do j = 0, Ly - 1
                 if ( mod(i * (Lx - 1) + j, 2) == 0  ) then
@@ -187,42 +217,47 @@ module Dynamic
                     Lattice(i, j) = rules2(newLattice(i, j))
                 end if
                 newLattice(i, j) = 0
-                ! Compute avg velocity components at current lattice site
-                vx = 0.; vy = 0.
 
-                do k = 0, 5
-                    ! If there is a particle going in a certain direction
-                    if ( iand(Lattice(i, j), ishft(int(1, int8), k)) /= 0 ) then
-                        ! Then add its velocity components to the total of the site
-                        vx = vx + ux(k+1)
-                        vy = vy + uy(k+1)
-                    end if
-                end do
-                avg_vel_site(i,j,1) = vx / 6.
-                avg_vel_site(i,j,2) = vy / 6.
+                ! Compute avg velocity components at current lattice site (only when we want to save)
+                if ( do_i_write ) then
+                    vx = 0.; vy = 0.
+                    do k = 0, 5
+                        ! If there is a particle going in a certain direction
+                        if ( iand(Lattice(i, j), ishft(int(1, int8), k)) /= 0 ) then
+                            ! Then add its velocity components to the total of the site
+                            vx = vx + ux(k+1)
+                            vy = vy + uy(k+1)
+                        end if
+                    end do
+                    avg_vel_site(i,j,1) = vx / 6.
+                    avg_vel_site(i,j,2) = vy / 6.
+                end if
             end do
         end do
-        !$OMP END PARALLEL DO
+
+        do j = 0, Ly - 1 ! I'm sure there is a better way of doing it
+            newLattice(0,j) = B; newLattice(Lx-1, j) = B
+        end do
 
         ! Now compute the 2D block average to reduce noise; this is what will be plotted as vector field
         norm = 1. / real(cell_size * cell_size) ! computed just once here instead of inside the nested loop
 
-        !$OMP PARALLEL DO DEFAULT(shared) PRIVATE(k, l, i, j, block_sum_x, block_sum_y)
-        do k = 0, Lx / cell_size - 1
-            do l = 0, Ly / cell_size - 1
-                block_sum_x = 0.0; block_sum_y = 0.0
+        if ( do_i_write ) then
+            do k = 0, Lx / cell_size - 1
+                do l = 0, Ly / cell_size - 1
+                    block_sum_x = 0.0; block_sum_y = 0.0
 
-                do i = k * cell_size, (k + 1) * cell_size - 1
-                    do j = l * cell_size, (l + 1) * cell_size - 1
-                        block_sum_x = block_sum_x + avg_vel_site(i, j, 1)
-                        block_sum_y = block_sum_y + avg_vel_site(i, j, 2)
+                    do i = k * cell_size, (k + 1) * cell_size - 1
+                        do j = l * cell_size, (l + 1) * cell_size - 1
+                            block_sum_x = block_sum_x + avg_vel_site(i, j, 1)
+                            block_sum_y = block_sum_y + avg_vel_site(i, j, 2)
+                        end do
                     end do
+                    avg_vel_cell(k, l, 1) = block_sum_x * norm
+                    avg_vel_cell(k, l, 2) = block_sum_y * norm
                 end do
-                avg_vel_cell(k, l, 1) = block_sum_x * norm
-                avg_vel_cell(k, l, 2) = block_sum_y * norm
             end do
-        end do
-        !$OMP END PARALLEL DO
+        end if
     end subroutine Evolve
 
     subroutine WriteVfield(Lx, Ly, avg_vel, it)
@@ -248,12 +283,13 @@ program FHP1
     use Variables
     use printing
     use Dynamic
-    use omp_lib
     implicit none
 
     integer :: Lx, Ly, it, nit, cell_size, frames
     integer(1), dimension(:,:), allocatable :: Lattice, newLattice
     real, dimension(:,:,:), allocatable :: avg_vel_site, avg_vel_cell
+    integer :: i
+    logical :: do_i_write
 
     character(len=32) :: arg
     call get_command_argument(1, arg); read(arg, *) Lx        ! Number of rows
@@ -276,18 +312,20 @@ program FHP1
     call InitLattice(Lattice, Lx, Ly)
 
     ! Evolution of the system
-    !call cpu_time(start_time)
-    start_time = omp_get_wtime()
+    call cpu_time(start_time)
     do it = 1, nit
-        call Evolve(Lattice, newLattice, Lx, Ly, avg_vel_site, avg_vel_cell, cell_size)
-        if ( mod(it, frames) == 0 ) then ! Writing to file is slow!!!
-            !$acc update host(avg_vel_site, avg_vel_cell)
+        do_i_write = mod(it, frames) == 0
+        call Evolve(Lattice, newLattice, Lx, Ly, avg_vel_site, avg_vel_cell, cell_size, do_i_write)
+        if ( do_i_write ) then ! Writing to file is slow!!!
             call WriteVfield(Lx/cell_size, Ly/cell_size, avg_vel_cell, it / frames)
         end if
     end do
-    !call cpu_time(end_time)
-    end_time = omp_get_wtime()
+    call cpu_time(end_time)
     print*, "Evolve time:", end_time - start_time
+
+    do i = 0, NUM_RULES
+        !print*, i, rules1(i), rules2(i)
+    end do
 
     !call stampatutto(Lattice, newLattice, Lx, Ly, avg_vel_site) ! This was for debugging
 
